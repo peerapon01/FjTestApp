ccb03bcb04df17f0ff1dc24c037ebd2de7437d66
Peerapon C <peerapon01@gmail.com>
2017-04-24 20:34:41 +0700

    complete

--- app/src/main/java/peerapon/me/fjtestapp/activity/MainActivity.java
+++ app/src/main/java/peerapon/me/fjtestapp/activity/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<List<ListResult>> call, Throwable t) {
-            
+
 
             }
         });
